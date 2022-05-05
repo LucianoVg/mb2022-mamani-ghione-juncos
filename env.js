50543c959297fb568cfde9a3f924f2ef577c7606
juncos22 <juncosnicolas36@gmail.com>
2022-05-05 09:11:05 -0300

    se agregaron las librerias de firebase y su configuracion y prisma client

---
+++ env.js
@@ -0,0 +1,2 @@
+export const urlDev = "http://localhost:3000"
+export const urlProd = ""
---
+++ firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1e4oeTZQN6I_zkvU8gOw0__ATjRKkIaI",
+    authDomain: "mb2020-mamani-ghione-juncos.firebaseapp.com",
+    projectId: "mb2020-mamani-ghione-juncos",
+    storageBucket: "mb2020-mamani-ghione-juncos.appspot.com",
+    messagingSenderId: "443959529787",
+    appId: "1:443959529787:web:4db5d190d596c100aee71e",
+    measurementId: "G-K6RYTN7XBC"
+};
+
+export const app = initializeApp(firebaseConfig) 
