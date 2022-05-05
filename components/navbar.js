31d8606c641ebf2fce1b490fc0475ed41903ebdf
LucianoVg <lucianovelazquez19@gmail.com>
2022-05-05 20:36:23 -0300

    boton hamburguesa a la izquierda

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,14 @@
 export default function Navbar() {
     return (
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-
+           
+            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle"><i className="fas fa-bars"></i></button>
+           
             <a className="navbar-brand ps-3" href="index.html">
                 {/* Agregar icono del instituto */}
             </a>
 
-            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle"><i className="fas fa-bars"></i></button>
+
 
             <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div className="input-group">
@@ -14,7 +16,7 @@ export default function Navbar() {
                     <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search"></i></button>
                 </div>
             </form>
-            
+
             <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="fas fa-user fa-fw"></i></a>
