116d1777cc6314d1b03157ce7e2cd19dc59b5a36
juncos22 <juncosnicolas36@gmail.com>
2022-07-30 17:52:16 -0300

    base de datos modificada

--- pages/api/gestion/cuenta.ts
+++ pages/api/gestion/cuenta.ts
@@ -5,35 +5,21 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const prisma = new PrismaClient()
     try {
-        const prisma = new PrismaClient()
-        // REVISAR CONSULTA
-        const asistenciaDeUsuarios = await prisma.asistencia.findMany({
-            select: {
-                tipoAsistencia: {
-                    select: {
-                        tipo: true,
-                        valor: true
-                    },
-                },
-                fecha: true,
-                usuario: {
-                    select: {
-                        nombre: true,
-                        apellido: true
-                    }
-                }
-            }
-        })
-        asistenciaDeUsuarios.map(ass => {
-            console.log(`Asistencia: ${ass.tipoAsistencia.tipo} 
-        - Valor: ${ass.tipoAsistencia.valor} 
-        - Fecha: ${ass.fecha} 
-        - Alumno: ${ass.usuario.nombre} ${ass.usuario.apellido}
-        - Preceptor: ${ass.usuario.nombre} ${ass.usuario.apellido}`)
-        })
 
-        return res.status(200).json(asistenciaDeUsuarios)
+        const asistencias = await prisma.asistencia.findMany()
+        console.log(asistencias);
+
+        // asistencias.map(ass => {
+        //     console.log(`Asistencia: ${ass.tipoAsistencia.tipo} 
+        // - Valor: ${ass.tipoAsistencia.valor} 
+        // - Fecha: ${ass.fecha} 
+        // - Alumno: ${ass.usuario.nombre} ${ass.usuario.apellido}
+        // - Preceptor: ${ass.usuario.nombre} ${ass.usuario.apellido}`)
+        // })
+
+        return res.status(200).json(asistencias)
     } catch (error) {
         return res.status(200).json(error)
     }
