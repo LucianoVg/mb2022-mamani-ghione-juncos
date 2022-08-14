93f66976e6b513ae5a915c5fe553fd0117dd5877
juncos22 <juncosnicolas36@gmail.com>
2022-08-14 12:28:04 -0300

    Update sidenav.tsx

--- components/sidenav.tsx
+++ components/sidenav.tsx
@@ -81,41 +81,29 @@ const Sidenav = () => {
                             Reportes
                             <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
                         </a>
-                        <div className="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                            <div className="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion2">
-                                <nav className="sb-sidenav-menu-nested nav">
+                        <div className="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion2">
+                            <nav className="sb-sidenav-menu-nested nav">
 
-                                    <Link href="/reportes/reporte_notas">
-                                        <a className="nav-link active" >Reporte Notas</a>
-                                    </Link>
-                                    <Link href="/reportes/reporte_asistencias">
-                                        <a className="nav-link active" >Reporte Asistencias</a>
-                                    </Link >
-                                    <Link href="/reportes/reporte_asistencia_Docente">
-                                        <a className="nav-link active" >Reporte Asistencia Docente</a>
-                                    </Link>
-                                    <Link href="/reportes/reporte_sanciones">
-                                        <a className="nav-link active" >Reporte Sanciones</a>
-                                    </Link>
-                                    <Link href="/reportes/reporte_preanalitico">
-                                        <a className="nav-link active" >Reporte Preanalitico</a>
-                                    </Link>
+                                <Link href="/reportes/reporte_notas">
+                                    <a className="nav-link active" >Reporte Notas</a>
+                                </Link>
+                                <Link href="/reportes/reporte_asistencias">
+                                    <a className="nav-link active" >Reporte Asistencias</a>
+                                </Link >
+                                <Link href="/reportes/reporte_asistencia_Docente">
+                                    <a className="nav-link active" >Reporte Asistencia Docente</a>
+                                </Link>
+                                <Link href="/reportes/reporte_sanciones">
+                                    <a className="nav-link active" >Reporte Sanciones</a>
+                                </Link>
+                                <Link href="/reportes/reporte_preanalitico">
+                                    <a className="nav-link active" >Reporte Preanalitico</a>
+                                </Link>
 
-                                    <Link href="/reportes/reporte_mejor_promedio">
-                                        <a className="nav-link active" >Reporte Mejor Promedio</a>
-                                    </Link>
-                                </nav >
-                            </div >
-
-                            <div className="sb-sidenav-menu-heading">Addons</div>
-                            <a className="nav-link" href="charts.html">
-                                <div className="sb-nav-link-icon"><i className="fas fa-chart-area"></i></div>
-                                Charts
-                            </a>
-                            <a className="nav-link" href="tables.html">
-                                <div className="sb-nav-link-icon"><i className="fas fa-table"></i></div>
-                                Tables
-                            </a>
+                                <Link href="/reportes/reporte_mejor_promedio">
+                                    <a className="nav-link active" >Reporte Mejor Promedio</a>
+                                </Link>
+                            </nav >
                         </div >
                     </div>
                 </div >
