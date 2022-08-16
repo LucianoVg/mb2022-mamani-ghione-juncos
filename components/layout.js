61daa1c2c9ebe7074dfd531006db81c4331d9e92
juncos22 <juncosnicolas36@gmail.com>
2022-08-16 17:45:35 -0300

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -6,31 +6,6 @@ import Script from "next/script";
 export const Layout = ({ children, title }) => {
     return (
         <>
-<<<<<<< Updated upstream
-        
-            <Script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-                integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-                crossOrigin="anonymous"></Script>
-
-            <Script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
-                integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
-                crossOrigin="anonymous"></Script>
-
-            <Script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
-                integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
-                crossOrigin="anonymous"></Script>
-
-            <Script src="/js/scripts.js"></Script>
-            
-            <Script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
-                    integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
-                    crossOrigin="anonymous"></Script>
-
-                <Script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
-                    integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
-                    crossOrigin="anonymous"></Script>
-=======
->>>>>>> Stashed changes
             <Head>
                 <meta charSet="utf-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
