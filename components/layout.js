ec2aae8cb2768f08c4b35bd63526ab7d8415a77b
LucianoVg <lucianovelazquez19@gmail.com>
2022-08-17 18:07:42 -0300

    layout link

--- components/layout.js
+++ components/layout.js
@@ -20,14 +20,19 @@ export const Layout = ({ children, title }) => {
     return (
         <>
             <Head>
-                <meta charSet="utf-8" />
+            <meta charSet="utf-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="deScription" content="Tesis del colegio Manuel Belgrano realizada por los integrantes Nicolas Mamani, Luciano Ghione y Nicolas Juncos." />
                 <meta name="author" content="Nicolas Mamani, Luciano Ghione, Nicolas Juncos" />
                 <title>{title}</title>
 
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    crossOrigin="anonymous" />
+
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
                 {/* <link rel="stylesheet" href="css/style5.css" /> */}
+             
 
             </Head>
             <Navbar />
@@ -44,3 +49,5 @@ export const Layout = ({ children, title }) => {
         </>
     )
 }
+
+
