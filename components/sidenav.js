dd1e94855aa63f6bc5b22652512b1559dc2aabd8
LucianoVg <lucianovelazquez19@gmail.com>
2022-08-17 17:30:06 -0300

    sidenav scroll listo

--- components/sidenav.js
+++ components/sidenav.js
@@ -14,74 +14,75 @@ const Sidenav = () => {
     }, [])
 
     return (
-        <div className="l-navbar" id="nav-bar">
-            <div>
-                <nav className="nav">
+
+        // scrolling-navbar
+        <div className="l-navbar " id="nav-bar">
+
+            <nav className="nav " id='parent'>
+
+                <div id='child'  >
                     <a className="nav_logo" href='/'>
                         {/* <i className='bx bx-bus-school nav_logo-icon'></i> */}
                         <Image style={{ borderRadius: 50 }} src={'/assets/img/logo.jpg'} width={25} height={25} />
                         <span className="nav_logo-name">Instituto <br />"El Salvador"</span>
                     </a>
 
-                    <div className="nav_list">
+                    <ul className="list-unstyled ps-0 overflow-hidden">
+                        <li >
+                            <a className=" nav_link btn btn-toggle align-items-center collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                                <i className='bx bx-grid-alt nav_icon'></i>
+                                Gestion
+                            </a>
+                            <div className="collapse ps-2" id="home-collapse">
+                                <ul className="list-unstyled small">
+                                    {
+                                        menus && menus.map((m, i) => (
+                                            !m.menu?.menuSistema.startsWith('Reporte') && (
+                                                <li key={i}>
+                                                    <a href={m.menu?.url} className="nav_link" >{m.menu?.menuSistema}</a>
+                                                </li>
+                                            )
+                                        ))
+                                    }
 
-                        <a className="nav_link" href='/'>
-                            <i className='bx bx-home nav_icon'></i>
-                            <span className="nav_name">Inicio</span>
-                        </a>
+                                </ul>
+                            </div>
+                        </li>
 
-                        <ul className="list-unstyled ps-0">
-                            <li>
-                                <a className="nav_link btn btn-toggle align-items-center collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
-                                    <i className='bx bx-bar-chart-square nav_icon'></i>
-                                    Gestion
-                                </a>
-                                <div className="collapse ps-2" id="home-collapse">
-                                    <ul className="list-unstyled small">
-                                        {
-                                            menus && menus.map((m, i) => (
-                                                !m.menu?.menuSistema.startsWith('Reporte') && (
-                                                    <li key={i}>
-                                                        <a href={m.menu?.url} className="nav_link" >{m.menu?.menuSistema}</a>
-                                                    </li>
-                                                )
-                                            ))
-                                        }
+                        <li >
+                            <a className=" nav_link btn btn-toggle align-items-center collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse-2" aria-expanded="false">
+                                <i className='bx bx-grid-alt nav_icon'></i>
+                                Reportes
+                            </a>
+                            <div className="collapse ps-2" id="home-collapse-2">
+                                <ul className="list-unstyled small">
+                                    {
+                                        menus && menus.map((m, i) => (
+                                            m.menu?.menuSistema.startsWith('Reporte') && (
+                                                <li key={i}>
+                                                    <a href={m.menu?.url} className="nav_link" >{m.menu?.menuSistema}</a>
+                                                </li>
+                                            )
+                                        ))
+                                    }
 
-                                    </ul>
-                                </div>
-                            </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
 
-                            <li>
-                                <a className=" nav_link btn btn-toggle align-items-center collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse-2" aria-expanded="false">
-                                    <i className='bx bx-file nav_icon'></i>
-                                    Reportes
-                                </a>
-                                <div className="collapse ps-2" id="home-collapse-2">
-                                    <ul className="list-unstyled small">
-                                        {
-                                            menus && menus.map((m, i) => (
-                                                m.menu?.menuSistema.startsWith('Reporte') && (
-                                                    <li key={i}>
-                                                        <a href={m.menu?.url} className="nav_link" >{m.menu?.menuSistema}</a>
-                                                    </li>
-                                                )
-                                            ))
-                                        }
 
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
+                    <a href="#" className="nav_link sb-sidenav-footer">
+                        <i className='bx bx-log-out nav_icon'></i>
+                        <span className="nav_name">Iniciar Sesion</span>
+                    </a>
 
-                        <a className="nav_link" href='/gestion/cuenta/login'>
-                            <i className="bx bx-lock-open"></i>
-                            <span className="nav_name">Iniciar Sesion</span>
-                        </a>
-                    </div>
+                </div >
 
-                </nav >
-            </div>
+
+
+
+            </nav>
         </div >
     )
 }
