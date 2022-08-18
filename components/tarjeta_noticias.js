0eeecd585a3a8ff332a5201be848c746833e5cac
LucianoVg <lucianovelazquez19@gmail.com>
2022-08-18 20:46:08 -0300

    cambios minimos

--- components/tarjeta_noticias.js
+++ components/tarjeta_noticias.js
@@ -10,7 +10,7 @@ const TarjetaNovedades = ({ url, titulo, descripcion }) => {
             <div className="card-body">
                 <h5 className="card-title">{titulo}</h5>
                 <p className="card-text">{descripcion}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <a href="#" className="btn btn-primary">Mas Informacion</a>
             </div>
         </div>
     )
