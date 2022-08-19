5eb0bcfa0f148573b943c87e9bb7d6e233f66616
LucianoVg <lucianovelazquez19@gmail.com>
2022-08-18 21:13:24 -0300

    Revert "tarjetas personalizadas"
    This reverts commit 98819d1568d174ec989f994dd50c5d5e51f78929.

--- components/tarjeta_noticias.js
+++ components/tarjeta_noticias.js
@@ -3,25 +3,16 @@ import styles from "../styles/tarjetaNoticias.module.css";
 const TarjetaNovedades = ({ url, titulo, descripcion }) => {
 
     return (
-        <div className={`${styles.html} ${styles.body} ${styles.all}`}>
-            <div className={`${styles.grid} `}>
-                <div className={styles.grid__item}>
-                    <div className={styles.card}>
-                        <img className={`${styles.card__img}`} src={url} alt="Snowy Mountains" />
-                        <div className={styles.card__content}>
-                            <h1 className={styles.card__header}>{titulo}</h1>
-                            <p className={styles.card__text}>{descripcion}
-                            </p>
-                            <button className={styles.card__btn}>Explore
-                                <span>&rarr;</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
+        <div className={`${styles.card}`} >
+            <img className={`${styles.card_img_top }`}
+                src={url}
+                alt="Card image cap" />
+            <div className="card-body">
+                <h5 className="card-title">{titulo}</h5>
+                <p className="card-text">{descripcion}</p>
+                <a href="#" className="btn btn-primary">Mas Informacion</a>
             </div>
         </div>
-
-
     )
 }
 
