df1c609b0e21fe84510d95b24882508c6e0cca7a
juncos22 <juncosnicolas36@gmail.com>
2022-08-22 17:23:12 -0300

    Update index.js

--- pages/gestion/notas/index.js
+++ pages/gestion/notas/index.js
@@ -19,24 +19,20 @@ export default function Notas() {
     return (
         <table className="table table-bordered">
             <thead>
-                {
-                    // <h1>{n.alumnoXcursoXdivision.usuario?.nombre}</h1>
-                    notas && notas.map((n, i) => (
-
-                        <tr key={i} >
-                            <th scope="col">{n.materia?.nombre}</th>
-                        </tr>
-
-                    ))
-                }
-
+                <tr>
+                    <th>Alumno</th>
+                    <th>Materia</th>
+                    <th>Nota</th>
+                </tr>
             </thead>
             <tbody>
                 {
                     notas && notas.map((n, i) => (
 
                         <tr key={i}>
-                            <td>{n.nota?.nota}</td>
+                            <td>{n.alumnoXCursoXDivision?.usuario?.nombre}</td>
+                            <td>{n.materia?.nombre}</td>
+                            <td>{n.nota}</td>
                         </tr>
                     ))
                 }
