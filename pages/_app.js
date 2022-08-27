60a6661b5f76a543f15b85443f236ffb4aaab77f
juncos22 <juncosnicolas36@gmail.com>
2022-08-27 18:33:07 -0300

    login nuevo diseño

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/styles.css'
 import '../styles/style5.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function MyApp({ Component, pageProps }) {
