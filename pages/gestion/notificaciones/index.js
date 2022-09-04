6f75d323701a9310555ad774510c638501fb4b0e
juncos22 <juncosnicolas36@gmail.com>
2022-09-04 18:53:59 -0300

    Update index.js

---
+++ pages/gestion/notificaciones/index.js
@@ -0,0 +1,9 @@
+import { Layout } from "../../../components/layout";
+
+export default function Notificaciones() {
+    return (
+        <Layout title={'Notificaciones'}>
+            <h3 className="text-center">Notificaciones</h3>
+        </Layout>
+    )
+}
