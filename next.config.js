8e974c5217796b3030dfbbab253b84e72ac83f9b
juncos22 <juncosnicolas36@gmail.com>
2022-09-04 19:36:49 -0300

    agregando los dominios permitidos

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['firebasestorage.googleapis.com']
+    domains: ['firebasestorage.googleapis.com', 'mb2022-mamani-ghione-juncos.vercel.app']
   },
   env: {
     BASE_URL: process.env.BASE_URL,
