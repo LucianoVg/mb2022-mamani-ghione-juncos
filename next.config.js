3ec9330922db6c430fc862a9993cc9d5ed735f54
juncos22 <juncosnicolas36@gmail.com>
2022-09-04 21:49:29 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   headers: {
-    basePath: process.env.BASE_URL,
+    basePath: process.env.NEXT_PUBLIC_CLIENT_URL,
     crossOrigin: '*'
   },
   images: {
     domains: ['firebasestorage.googleapis.com']
   },
   env: {
-    BASE_URL: process.env.BASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    BASE_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
