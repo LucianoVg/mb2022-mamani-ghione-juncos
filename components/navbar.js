4ef771d625cd8eb69877bfd3ee4444b7cb822437
juncos22 <juncosnicolas36@gmail.com>
2022-09-06 21:37:35 -0300

    separando codigo

--- components/navbar.js
+++ components/navbar.js
@@ -1,28 +1,73 @@
 import Link from "next/link"
 import { useAuth } from "./context/authUserProvider"
+import { Toolbar, IconButton, Typography } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from '@mui/icons-material/Menu'
+import MuiAppBar from '@mui/material/AppBar';
+import { styled } from '@mui/material/styles';
+import theme from "../src/theme";
 
-export const Navbar = () => {
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: 240,
+        width: `calc(100% - ${240}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+export const Navbar = ({ toggleDrawer, open }) => {
     const { loading, authUser } = useAuth()
     return (
-        <header className="header " id="header">
-            <div className="header_toggle">
-                <i className='bx bx-menu' id="header-toggle"></i>
-            </div>
-            <Link href={'/gestion/institucional'}>
-                <a className="ms-auto p-2">
-                    Institucional
-                </a>
-            </Link>
-            {
-                !loading && authUser && (
-                    <div onClick={(e) => console.log('Ir al perfil')} className="header_img bg-secondary" style={{ width: 30, height: 30, cursor: 'pointer' }}>
-                        <i className='bx bx-user m-auto'></i>
-                        {/* <span>
-                            {authUser.email}
-                        </span> */}
-                    </div>
-                )
-            }
-        </header>
+        <>
+            <AppBar position="absolute" open={open}>
+                <Toolbar
+                    sx={{
+                        pr: '24px', // keep right padding when drawer closed
+                    }}
+                >
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer}
+                        sx={{
+                            marginRight: '36px',
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        component="h1"
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Instituto Privado &quot;El Salvador&quot;
+                    </Typography>
+
+                    <Typography href='/gestion/institucional' component={'a'} variant="body1" sx={{ mr: 1, textDecoration: 'none' }} color='white'>
+                        Institucional
+                    </Typography>
+
+                    {
+                        !loading && authUser && (
+                            <AccountCircleIcon />
+                        )
+                    }
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
---
+++ components/sidebar.js
@@ -0,0 +1,160 @@
+import axios from 'axios'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useAuth } from './context/authUserProvider'
+import MuiDrawer from '@mui/material/Drawer';
+import theme from '../src/theme'
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
+import { styled } from '@mui/material/styles';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import { HomeOutlined, InfoOutlined } from '@mui/icons-material';
+import { ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import React from 'react'
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        '& .MuiDrawer-paper': {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: 240,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            boxSizing: 'border-box',
+            ...(!open && {
+                overflowX: 'hidden',
+                transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                }),
+                width: theme.spacing(7),
+                [theme.breakpoints.up('sm')]: {
+                    width: theme.spacing(9),
+                },
+            }),
+        },
+    }),
+);
+
+const Sidebar = ({ open, toggleDrawer }) => {
+    const [menus, setMenus] = useState([])
+    const router = useRouter()
+    const { loading, authUser, cerrarSesion } = useAuth()
+
+    useEffect(() => {
+        if (!loading && authUser) {
+            axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/cuenta/${authUser?.email}`)
+                .then(res => {
+                    if (res.data) {
+                        axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/submenu/${res.data?.rol?.id}`)
+                            .then(r => {
+                                console.log(r.data);
+                                setMenus(r.data)
+                            })
+                    }
+                })
+        }
+    }, [authUser, loading])
+
+    const logout = () => {
+        cerrarSesion()
+            .then(() => {
+                router.reload()
+            })
+    }
+
+    return (
+        <Drawer variant="permanent" open={open}>
+            <Toolbar
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
+                    px: [1],
+                }}
+            >
+                <IconButton onClick={toggleDrawer}>
+                    <ChevronLeftIcon />
+                </IconButton>
+            </Toolbar>
+            <List component="nav">
+                <React.Fragment>
+                    <ListItemButton onClick={() => router.push('/')}>
+                        <ListItemIcon>
+                            <HomeOutlined />
+                        </ListItemIcon>
+                        <ListItemText primary="Inicio" />
+                    </ListItemButton>
+                    <Divider sx={{ mt: 1, mb: 1 }} />
+
+                    {
+                        menus && (
+                            <Typography ml={1} style={{ fontWeight: 'bold' }}>Gestion</Typography>
+                        )
+                    }
+                    {
+                        menus && menus.map((m, i) => (
+                            <ListItemButton key={i} onClick={() => router.push(m?.menu?.url)}>
+                                <ListItemIcon>
+                                    {
+                                        m?.menu?.menuSistema === 'Usuarios' && <AssignmentIndOutlinedIcon />
+                                    }
+                                    {
+                                        m?.menu?.menuSistema === 'Asistencias' && <ContentPasteSearchOutlinedIcon />
+                                    }
+                                </ListItemIcon>
+                                <ListItemText primary={m?.menu?.menuSistema} />
+                            </ListItemButton>
+                        ))
+                    }
+
+                    {
+                        authUser && (
+                            <ListItemButton onClick={logout}>
+                                <ListItemIcon>
+                                    <LogoutIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Cerrar Sesion" />
+                            </ListItemButton>
+                        )
+                    }
+                    {
+                        !authUser && (
+                            <ListItemButton onClick={() => router.push('/gestion/cuenta/login')}>
+                                <ListItemIcon>
+                                    <LoginIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Iniciar Sesion" />
+                            </ListItemButton>
+                        )
+                    }
+                    {/* <Typography style={{ fontWeight: 'bold' }}>Reportes</Typography>
+            <ListItemButton onClick={() => router.push('/')}>
+                <ListItemIcon>
+                    <HomeOutlined />
+                </ListItemIcon>
+                <ListItemText primary="Usuarios" />
+            </ListItemButton>
+            <ListItemButton onClick={() => router.push('/about')}>
+                <ListItemIcon>
+                    <InfoOutlined />
+                </ListItemIcon>
+                <ListItemText primary="Sanciones" />
+            </ListItemButton> */}
+                </React.Fragment>
+            </List>
+        </Drawer>
+    )
+}
+
+export default Sidebar
+
