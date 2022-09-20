a792b0a0760c843cf91aaa448d180bdd7049b214
LucianoVg <lucianovelazquez19@gmail.com>
2022-09-19 23:17:02 -0300

    notificaciones responsivo

--- pages/gestion/notificaciones/index.js
+++ pages/gestion/notificaciones/index.js
@@ -33,22 +33,86 @@ const Notificaciones = () => {
         <Layout title='Enviar Notificaciones'>
             {/* <Notificacion /> */}
             <Container style={{ position: 'relative', }}>
-                <h1 className="text-center">Enviar Notificacion</h1>
+
                 <Grid container
-                    style={{ position: 'absolute', marginTop: '-85px', }}
-                    direction="column"
-                    justifyContent="flex-start"
-                    alignItems="flex-end"
-
-
                 >
+                    <Grid item xs>
+                        <h1 className="text-center">Enviar Notificacion</h1>
+                        <Box direction="column">
+                            <Box direction='row'>
+                                <FormControl>
+                                    <InputLabel id="demo-simple-select-label">Curso</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={age}
+                                        name="age"
+                                        label="Age"
+                                        onChange={handleChange}
+                                        sx={{width: '90px', marginRight: '20px'}}
+                                    >
+                                        <MenuItem value={10}>Todos</MenuItem>
+                                        <MenuItem value={1}>1 A</MenuItem>
+                                        <MenuItem value={2}>1 B</MenuItem>
+                                        <MenuItem value={3}>2 A</MenuItem>
+                                        <MenuItem value={4}>2 B</MenuItem>
+                                        <MenuItem value={5}>3 A</MenuItem>
+                                        <MenuItem value={6}>3 B</MenuItem>
+                                        <MenuItem value={5}>4 A</MenuItem>
+                                        <MenuItem value={6}>4 B</MenuItem>
+                                        <MenuItem value={5}>5 A</MenuItem>
+                                        <MenuItem value={6}>5 B</MenuItem>
+                                        <MenuItem value={5}>6 A</MenuItem>
+                                        <MenuItem value={6}>6 B</MenuItem>
+                                    </Select>
+
+                                </FormControl>
+                                <FormControl fullWidth sx={{ width: '30%', marginRight: '20px' }}>
+                                    <InputLabel id="demo-simple-select-label">Usuario</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={nombre}
+                                        name="nombre"
+                                        label="nombre"
+                                        onChange={handleNombre}
+                                        sx={{width: '150px'}}
+                                    >
+                                        <MenuItem value={'JuanJuncos'}>Juan Juncos</MenuItem>
+                                        <MenuItem value={'SamuelBertola'}>Samuel Bertola</MenuItem>
+                                        <MenuItem value={'LuisGarcia'}>Luis Garcia</MenuItem>
+
+                                    </Select>
+                                </FormControl>
+                            </Box>
+
+                          
+                            <h2><strong>Asunto</strong></h2>
+                            <TextareaAutosize
+
+                                style={{ width: '400px', height: '35px', resize: 'none', fontSize: '20px' }}
+                            />
+                            <h2><strong>Contenido</strong></h2>
+                            <TextareaAutosize
+
+                                style={{ width: '400px', height: '200px', resize: 'none', fontSize: '20px' }}
+                            />
+
+                          <Box xs={12}>
+                          <Button variant="contained">Enviar</Button>
+                          </Box>
+
+                        </Box>
+
+
+                    </Grid>
                     <Grid item xs={4}>
                         <h1>Notificaciones enviadas</h1>
                         <List style={{ backgroundColor: 'white', border: '0 10px 15px black', borderRadius: '10px' }}>
                             <ListItem disablePadding
                             >
-                                <ListItemButton   component="a" href="#simple-list">
-                                    <ListItemText primary="Trash"  />
+                                <ListItemButton component="a" href="#simple-list">
+                                    <ListItemText primary="Trash" />
 
                                 </ListItemButton>
                             </ListItem>
@@ -71,72 +135,8 @@ const Notificaciones = () => {
                     </Grid>
 
                 </Grid>
-                <Grid container spacing={2}>
 
-                    <Grid item xs={2}>
-                        <FormControl fullWidth sx={{ width: '70%' }}>
-                            <InputLabel id="demo-simple-select-label">Curso</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={age}
-                                name="age"
-                                label="Age"
-                                onChange={handleChange}
-                            >
-                                <MenuItem value={10}>Todos</MenuItem>
-                                <MenuItem value={1}>1 A</MenuItem>
-                                <MenuItem value={2}>1 B</MenuItem>
-                                <MenuItem value={3}>2 A</MenuItem>
-                                <MenuItem value={4}>2 B</MenuItem>
-                                <MenuItem value={5}>3 A</MenuItem>
-                                <MenuItem value={6}>3 B</MenuItem>
-                                <MenuItem value={5}>4 A</MenuItem>
-                                <MenuItem value={6}>4 B</MenuItem>
-                                <MenuItem value={5}>5 A</MenuItem>
-                                <MenuItem value={6}>5 B</MenuItem>
-                                <MenuItem value={5}>6 A</MenuItem>
-                                <MenuItem value={6}>6 B</MenuItem>
-                            </Select>
 
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <FormControl fullWidth sx={{ width: '30%' }}>
-                            <InputLabel id="demo-simple-select-label">Nombre</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={nombre}
-                                name="nombre"
-                                label="nombre"
-                                onChange={handleNombre}
-                            >
-                                <MenuItem value={'JuanJuncos'}>Juan Juncos</MenuItem>
-                                <MenuItem value={'SamuelBertola'}>Samuel Bertola</MenuItem>
-                                <MenuItem value={'LuisGarcia'}>Luis Garcia</MenuItem>
-
-                            </Select>
-
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12} >
-                        <h2><strong>Asunto</strong></h2>
-                        <TextareaAutosize
-
-                            style={{ width: '400px', height: '35px', resize: 'none', fontSize: '20px' }}
-                        />
-                        <h2><strong>Contenido</strong></h2>
-                        <TextareaAutosize
-
-                            style={{ width: '400px', height: '200px', resize: 'none', fontSize: '20px' }}
-                        />
-                    </Grid>
-                    <Grid item xs={12} disablePadding>
-                        <Button variant="contained">Enviar</Button>
-                    </Grid>
-
-                </Grid>
             </Container>
 
         </Layout >
