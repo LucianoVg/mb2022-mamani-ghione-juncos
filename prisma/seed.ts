1c6e790859acca0752a5dad9823ac5c0984b3b5e
juncos22 <juncosnicolas36@gmail.com>
2022-10-15 21:53:21 -0300

    seed de enfermedades

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,28 +10,20 @@ import { notas } from './seeds/notas';
 import { noticias } from './seeds/noticias';
 import { tiposSancion } from './seeds/tiposSancion';
 import { trimestres } from './seeds/trimestres';
+import { enfermedades } from "./seeds/enfermedad";
 
 const prisma = new PrismaClient();
 
 async function main() {
-    asistencias.map(async (a) => {
-        const asistencia = await prisma.asistencia.create({
-            data: {
-                creadoEn: a.creadaEn,
-                actualizadoEn: "",
-                presente: a.presente,
-                ausente: a.ausente,
-                llegadaTarde: a.llegadaTarde,
-                llegadaTardeJustificada: a.llegadaTardeJustificada,
-                mediaFalta: a.mediaFalta,
-                mediaFaltaJustificada: a.mediaFaltaJustificada,
-                idUsuario: a.idUsuario,
-                ausenteJustificado: a.ausenteJustificado,
-                idAlumnoXcursoXdivision: a.idAlumno
-            }
-        })
-        console.log(asistencia);
-    })
+    // enfermedades.map(async (e) => {
+    //     const enfermedad = await prisma.enfermedad.create({
+    //         data: {
+    //             descripcion: e.nombre,
+    //             usuarioId: e.idUsuario
+    //         }
+    //     })
+    //     console.log(enfermedad);
+    // })
 }
 
 main()
