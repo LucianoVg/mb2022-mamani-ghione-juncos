7abae465391bdd500b18a4e8ce133d7a9ad3c5e4
juncos22 <juncosnicolas36@gmail.com>
2022-10-15 22:03:46 -0300

    Update curso.js

--- pages/api/gestion/institucional/curso.js
+++ pages/api/gestion/institucional/curso.js
@@ -1,12 +1,12 @@
 import { TraerNotas } from "../../../../servicios/notas";
-import { ListarCurso } from "../../../../servicios/notas";
+// import { ListarCurso } from "../../../../servicios/notas";
 export default async function handler(
     req,
     res
 ) {
     try {
-        const cursos = await ListarCurso()
-        return res.status(200).json(cursos)
+        // const cursos = await ListarCurso()
+        // return res.status(200).json(cursos)
     } catch (error) {
         return res.status(200).json({ mensaje: error.message })
     }
