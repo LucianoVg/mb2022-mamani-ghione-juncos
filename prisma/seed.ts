c5f67863c86335ec098871caf55f2cc91ae60ca8
LucianoVg <lucianovelazquez19@gmail.com>
2022-10-17 19:53:59 -0300

    seeders

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,6 +11,8 @@ import { noticias } from './seeds/noticias';
 import { tiposSancion } from './seeds/tiposSancion';
 import { trimestres } from './seeds/trimestres';
 import { enfermedades } from "./seeds/enfermedad";
+import { usuarios } from "./seeds/usuarios";
+import { alumnoXcursoXdivision } from "./seeds/alumnoXcursoXdivision";
 
 const prisma = new PrismaClient();
 
@@ -23,6 +25,35 @@ async function main() {
     //         }
     //     })
     //     console.log(enfermedad);
+    // })
+    alumnoXcursoXdivision.map(async (a) => {
+        const alumnoXcursoXdivision = await prisma.alumnoXcursoXdivision.create({
+            data: {
+                anoActual: a.anoActual,
+                idCursoXdivision: a.idCursoXdivision,
+                idUsuario: a.idUsuario,
+                idEstadoAlumno: a.idEstadoAlumno
+            }
+        })
+        console.log(alumnoXcursoXdivision);
+    })
+    // usuarios.map(async (u) => {
+    //     const usuario = await prisma.usuario.create({
+    //         data: {
+    //             login: u.login,
+    //             password: u.password,
+    //             nombre: u.nombre,
+    //             apellido: u.apellido,
+    //             correo: u.correo,
+    //             idRol: u.idRol ,
+    //             legajo: u.legajo,
+    //             sexo: u.sexo,
+    //             localidad: u.localidad ,
+    //             telefono: u.telefono,
+    //             direccion: u.direccion
+    //         }
+    //     })
+    //     console.log(usuario);
     // })
 }
 
