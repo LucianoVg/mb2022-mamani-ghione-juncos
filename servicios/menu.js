2be089ad15d42e2a79d3da486cd998f09fad6039
juncos22 <juncosnicolas36@gmail.com>
2022-11-20 12:31:55 -0300

    Update menu.js

--- servicios/menu.js
+++ servicios/menu.js
@@ -8,7 +8,7 @@ export async function FiltrarMenu(idRol, prefijoUrl) {
     },
     where: {
       AND: [
-        { idRol: idRol },
+        { idRol: Number(idRol) },
         {
           menu: {
             url: {
