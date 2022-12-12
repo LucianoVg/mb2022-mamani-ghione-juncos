1daac54dd9304fe43513c5ad83c4f8c86888a874
LucianoVg <lucianovelazquez19@gmail.com>
2022-12-12 20:42:22 -0300

    a

--- pages/api/servicios/notas.js
+++ pages/api/servicios/notas.js
@@ -70,6 +70,173 @@ export async function notasTrimestres(idAlumno, idMateria) {
         console.log(error);
     }
 }
+
+
+export async function contarNotas(idMateria) {
+    try {
+        const conteo = await Prisma.newPrisma.$queryRaw`SELECT n.idmateria,
+        (
+             (select count(*) from nota where   nota1= 1   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 1    and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 1  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota4= 1   and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 1  and idmateria =${Number(idMateria)}) 
+        ) as nota1,
+        (
+             (select count(*) from nota where   nota1= 2  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 2  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 2  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota4= 2   and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 2  and idmateria =${Number(idMateria)}) 
+        ) as nota2,
+        (
+             (select count(*) from nota where   nota1= 3   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 3  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 3  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota4=3   and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 3  and idmateria =${Number(idMateria)}) 
+        ) as nota3,
+        (
+             (select count(*) from nota where   nota1= 4   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 4  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 4  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota4= 4   and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 4 and idmateria =${Number(idMateria)}) 
+        ) as nota4,
+        (
+             (select count(*) from nota where   nota1= 5   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 5  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 5  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota4= 5  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 5  and idmateria =${Number(idMateria)}) 
+        ) as nota5,
+        (
+             (select count(*) from nota where   nota1= 6   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 6 and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 6  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota4= 6   and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 6  and idmateria =${Number(idMateria)}) 
+        ) as nota6,
+        (
+             (select count(*) from nota where   nota1= 7   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 7  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 7  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota4= 7   and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 7 and idmateria =${Number(idMateria)}) 
+        ) as nota7,
+        (
+             (select count(*) from nota where   nota1= 8   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 8  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 8  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota4= 8   and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 8  and idmateria =${Number(idMateria)}) 
+        ) as nota8,
+        (
+             (select count(*) from nota where   nota1= 9   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 9  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 9  and idmateria =${Number(idMateria)})  +
+             (select count(*) from nota where   nota4= 9   and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where    nota5= 9  and idmateria =${Number(idMateria)}) 
+        ) as nota9,
+        (
+             (select count(*) from nota where   nota1= 10   and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota2= 10 and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota3= 10  and idmateria =${Number(idMateria)})+
+             (select count(*) from nota where   nota4= 10  and idmateria =${Number(idMateria)}) +
+             (select count(*) from nota where   nota5= 10  and idmateria =${Number(idMateria)}) 
+        ) as nota10
+        FROM historialnota as n
+        where idmateria= ${Number(idMateria)}
+        group by n.idmateria`
+
+        var string = JSON.stringify(conteo, (_, v) => typeof v === 'bigint' ? v.toString() : v)
+        let notas = JSON.parse(string)
+        return notas
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export async function TraerNotas(options) {
+    try {
+        return await Prisma.newPrisma.nota.findMany(options)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+export async function updateNota(idNota, nota1, nota2, nota3, nota4, nota5,
+    columna1, columna2, columna3, columna4, columna5) {
+    try {
+        let notas = []
+        if (columna1) {
+            const newNota1 = await Prisma.newPrisma.nota.update({
+                data: {
+                    nota1: Number(nota1)
+
+                },
+                where: {
+                    id: Number(idNota)
+                }
+            })
+            notas.push(newNota1)
+        }
+        if (columna2) {
+            const newNota2 = await Prisma.newPrisma.nota.update({
+                data: {
+                    nota2: Number(nota2)
+
+                },
+                where: {
+                    id: Number(idNota)
+                }
+            })
+            notas.push(newNota2)
+        }
+        if (columna3) {
+            const newNota3 = await Prisma.newPrisma.nota.update({
+                data: {
+                    nota3: Number(nota3)
+
+                },
+                where: {
+                    id: Number(idNota)
+                }
+            })
+            notas.push(newNota3)
+        }
+        if (columna4) {
+            const newNota4 = await Prisma.newPrisma.nota.update({
+                data: {
+                    nota4: Number(nota4)
+                },
+                where: {
+                    id: Number(idNota)
+                }
+            })
+            notas.push(newNota4)
+        }
+        if (columna5) {
+            const newNota5 = await Prisma.newPrisma.nota.update({
+                data: {
+                    nota5: Number(nota5)
+                },
+                where: {
+                    id: Number(idNota)
+                }
+            })
+            notas.push(newNota5)
+        }
+        return notas
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 // export async function contarNotas() {
 //     try {
 //         const nota1_1 = await Prisma.newPrisma.historialnota.aggregate({
@@ -1050,167 +1217,3 @@ export async function notasTrimestres(idAlumno, idMateria) {
 //         console.log(error);
 //     }
 // }
-
-export async function contarNotas(idMateria) {
-    try {
-        const conteo = await Prisma.newPrisma.$queryRaw`SELECT n.idmateria,
-        (
-             (select count(*) from nota where   nota1= 1   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 1    and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 1  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota4= 1   and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 1  and idmateria =${Number(idMateria)}) 
-        ) as nota1,
-        (
-             (select count(*) from nota where   nota1= 2  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 2  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 2  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota4= 2   and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 2  and idmateria =${Number(idMateria)}) 
-        ) as nota2,
-        (
-             (select count(*) from nota where   nota1= 3   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 3  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 3  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota4=3   and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 3  and idmateria =${Number(idMateria)}) 
-        ) as nota3,
-        (
-             (select count(*) from nota where   nota1= 4   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 4  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 4  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota4= 4   and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 4 and idmateria =${Number(idMateria)}) 
-        ) as nota4,
-        (
-             (select count(*) from nota where   nota1= 5   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 5  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 5  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota4= 5  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 5  and idmateria =${Number(idMateria)}) 
-        ) as nota5,
-        (
-             (select count(*) from nota where   nota1= 6   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 6 and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 6  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota4= 6   and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 6  and idmateria =${Number(idMateria)}) 
-        ) as nota6,
-        (
-             (select count(*) from nota where   nota1= 7   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 7  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 7  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota4= 7   and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 7 and idmateria =${Number(idMateria)}) 
-        ) as nota7,
-        (
-             (select count(*) from nota where   nota1= 8   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 8  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 8  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota4= 8   and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 8  and idmateria =${Number(idMateria)}) 
-        ) as nota8,
-        (
-             (select count(*) from nota where   nota1= 9   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 9  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 9  and idmateria =${Number(idMateria)})  +
-             (select count(*) from nota where   nota4= 9   and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where    nota5= 9  and idmateria =${Number(idMateria)}) 
-        ) as nota9,
-        (
-             (select count(*) from nota where   nota1= 10   and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota2= 10 and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota3= 10  and idmateria =${Number(idMateria)})+
-             (select count(*) from nota where   nota4= 10  and idmateria =${Number(idMateria)}) +
-             (select count(*) from nota where   nota5= 10  and idmateria =${Number(idMateria)}) 
-        ) as nota10
-        FROM historialnota as n
-        where idmateria= ${Number(idMateria)}
-        group by n.idmateria`
-
-        var string = JSON.stringify(conteo, (_, v) => typeof v === 'bigint' ? v.toString() : v)
-        let notas = JSON.parse(string)
-        return notas
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-export async function TraerNotas(options) {
-    try {
-        return await Prisma.newPrisma.nota.findMany(options)
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-
-export async function updateNota(idNota, nota1, nota2, nota3, nota4, nota5,
-    columna1, columna2, columna3, columna4, columna5) {
-    try {
-        let notas = []
-        if (columna1) {
-            const newNota1 = await Prisma.newPrisma.nota.update({
-                data: {
-                    nota1: Number(nota1)
-
-                },
-                where: {
-                    id: Number(idNota)
-                }
-            })
-            notas.push(newNota1)
-        }
-        if (columna2) {
-            const newNota2 = await Prisma.newPrisma.nota.update({
-                data: {
-                    nota2: Number(nota2)
-
-                },
-                where: {
-                    id: Number(idNota)
-                }
-            })
-            notas.push(newNota2)
-        }
-        if (columna3) {
-            const newNota3 = await Prisma.newPrisma.nota.update({
-                data: {
-                    nota3: Number(nota3)
-
-                },
-                where: {
-                    id: Number(idNota)
-                }
-            })
-            notas.push(newNota3)
-        }
-        if (columna4) {
-            const newNota4 = await Prisma.newPrisma.nota.update({
-                data: {
-                    nota4: Number(nota4)
-                },
-                where: {
-                    id: Number(idNota)
-                }
-            })
-            notas.push(newNota4)
-        }
-        if (columna5) {
-            const newNota5 = await Prisma.newPrisma.nota.update({
-                data: {
-                    nota5: Number(nota5)
-                },
-                where: {
-                    id: Number(idNota)
-                }
-            })
-            notas.push(newNota5)
-        }
-        return notas
-    } catch (error) {
-        console.log(error);
-    }
-}
