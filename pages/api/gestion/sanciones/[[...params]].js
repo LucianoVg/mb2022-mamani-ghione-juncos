e54c120691cf57b63e9aa67911fe9c5316abf30f
LucianoVg <lucianovelazquez19@gmail.com>
2022-12-11 23:47:49 -0300

    Update [[...params]].js

--- pages/api/gestion/sanciones/[[...params]].js
+++ pages/api/gestion/sanciones/[[...params]].js
@@ -33,7 +33,7 @@ export default async function hORler(req, res) {
                 },
                 orderBy: {
                     sancion: {
-                        fecha: 'desc'
+                        id: 'desc'
                     }
                 }
             }
