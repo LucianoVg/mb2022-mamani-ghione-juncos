d4c60022a71b09b4fe58721bb5edca6bc4622deb
juncos22 <juncosnicolas36@gmail.com>
2022-12-12 10:28:00 -0300

    Revert "quitando cosas"
    This reverts commit c60b9e9bbdb514249f98973fe6edc0f95c759d83.

---
+++ pages/gestion/estudiantes/material_estudio.jsx
@@ -0,0 +1,12 @@
+
+import { Layout } from "../../../components/layout";
+
+const MaterialDeEstudio = () => {
+    return (
+        <Layout title={'Material de Estudio'}>
+            <h1>Material de Estudio</h1>
+        </Layout>
+    )
+}
+
+export default MaterialDeEstudio;
