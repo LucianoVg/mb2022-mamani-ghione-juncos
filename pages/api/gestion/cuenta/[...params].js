89970cadecbc29b8d2309943fe5378d8b60fea92
juncos22 <juncosnicolas36@gmail.com>
2023-05-04 11:06:48 -0300

    actualizacion prisma

--- pages/api/gestion/cuenta/[...params].js
+++ pages/api/gestion/cuenta/[...params].js
@@ -1,42 +1,36 @@
 import NextCors from "nextjs-cors/dist";
 import { db } from "../../../../prisma";
 
-export default async function handler(
-    req,
-    res
-) {
-    try {
-        await NextCors(req, res, {
-            // Options
-            methods: ['GET'],
-            origin: process.env.HOST,
-            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-        });
-        if (req.method === 'GET') {
-            const { params } = req.query
-            const usuario = await traerUsuario(params[0], params[1])
-            return res.status(200).json(usuario)
-        }
-    } catch (error) {
-        return res.status(200).json({ mensaje: error.message })
+export default async function handler(req, res) {
+  try {
+    await NextCors(req, res, {
+      // Options
+      methods: ["GET"],
+      origin: process.env.HOST,
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+    if (req.method === "GET") {
+      const { params } = req.query;
+      const usuario = await traerUsuario(params[0], params[1]);
+      return res.status(200).json(usuario);
     }
+  } catch (error) {
+    return res.status(200).json({ mensaje: error.message });
+  }
 }
 
 export async function traerUsuario(correo, password) {
-    try {
-        const usuario = await db.usuario.findFirst({
-            include: {
-                rol: true
-            },
-            where: {
-                AND: [
-                    { correo: correo },
-                    { password: password }
-                ]
-            }
-        })
-        return usuario
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const usuario = await db.usuario.findFirst({
+      include: {
+        rol: true,
+      },
+      where: {
+        AND: [{ correo: correo }, { password: password }],
+      },
+    });
+    return usuario;
+  } catch (error) {
+    console.log(error);
+  }
 }
--- pages/api/gestion/cuenta/index.js
+++ pages/api/gestion/cuenta/index.js
@@ -2,183 +2,232 @@ import NextCors from "nextjs-cors/dist";
 import { db } from "../../../../prisma";
 
 export default async function handler(req, res) {
-    try {
-        await NextCors(req, res, {
-            // Options
-            methods: ['POST'],
-            origin: process.env.HOST,
-            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-        });
-        if (req.method === 'POST') {
-            const { login,
-                nombre, apellido, legajo,
-                telefono, correo, direccion,
-                localidad, idRol, idTutor,
-                sexo, contrasenia, idCurso,
-                idUsuario, esAlumno, esDocente, idMaterias } = req.body
+  try {
+    await NextCors(req, res, {
+      // Options
+      methods: ["POST"],
+      origin: process.env.HOST,
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+    if (req.method === "POST") {
+      const {
+        login,
+        nombre,
+        apellido,
+        legajo,
+        telefono,
+        correo,
+        direccion,
+        localidad,
+        idRol,
+        idTutor,
+        sexo,
+        contrasenia,
+        idCurso,
+        idUsuario,
+        esAlumno,
+        esDocente,
+        idMaterias,
+      } = req.body;
 
-            console.log({
-                login,
-                nombre, apellido, legajo,
-                telefono, correo, direccion,
-                localidad, idRol, idTutor,
-                sexo, contrasenia, idCurso,
-                idUsuario, esAlumno, esDocente, idMaterias
-            });
-            const creado = await registrarUsuario(login, nombre, apellido,
-                correo, legajo, telefono,
-                localidad, direccion, idRol,
-                idTutor, contrasenia, sexo, idCurso,
-                idUsuario, esAlumno, esDocente, idMaterias)
+      console.log({
+        login,
+        nombre,
+        apellido,
+        legajo,
+        telefono,
+        correo,
+        direccion,
+        localidad,
+        idRol,
+        idTutor,
+        sexo,
+        contrasenia,
+        idCurso,
+        idUsuario,
+        esAlumno,
+        esDocente,
+        idMaterias,
+      });
+      const creado = await registrarUsuario(
+        login,
+        nombre,
+        apellido,
+        correo,
+        legajo,
+        telefono,
+        localidad,
+        direccion,
+        idRol,
+        idTutor,
+        contrasenia,
+        sexo,
+        idCurso,
+        idUsuario,
+        esAlumno,
+        esDocente,
+        idMaterias
+      );
 
-            return res.status(200).json(creado)
-        } else {
-            return res.status(400).json({ mensaje: "Metodo no permitido" })
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send(error)
+      return res.status(200).json(creado);
+    } else {
+      return res.status(400).json({ mensaje: "Metodo no permitido" });
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(error);
+  }
 }
 
 export async function registrarUsuario(
-    login, nombre, apellido, correo,
-    legajo, telefono, localidad,
-    direccion, idRol, idTutor = 0,
-    contrasenia, sexo, idCurso = 0,
-    idUsuario, esAlumno = false, esDocente = false, idMaterias) {
-
-    try {
-        if (esAlumno) {
-            const estadoalumno = await db.estadoalumno.findFirst({
-                where: {
-                    estado: 'Regular'
-                }
-            })
-            let alumno = await db.usuario.create({
-                data: {
-                    login: login,
-                    nombre: nombre,
-                    apellido: apellido,
-                    legajo: legajo,
-                    correo: correo,
-                    localidad: localidad,
-                    telefono: telefono,
-                    direccion: direccion,
-                    idrol: Number(idRol),
-                    sexo: sexo,
-                    password: contrasenia,
-                    alumnoxcursoxdivision: {
-                        create: {
-                            tutor: {
-                                connect: {
-                                    id: Number(idTutor)
-                                }
-                            },
-                            cursoxdivision: {
-                                connect: {
-                                    id: Number(idCurso)
-                                }
-                            },
-                            estadoalumno: {
-                                connect: {
-                                    id: Number(estadoalumno?.id)
-                                }
-                            },
-                            fechamatriculacion: new Date().toLocaleDateString('es-AR').split('T')[0],
-                            asistencia: {
-                                create: {
-                                    presente: false,
-                                    ausente: false,
-                                    ausentejustificado: false,
-                                    llegadatarde: false,
-                                    llegadatardejustificada: false,
-                                    mediafalta: false,
-                                    mediafaltajustificada: false,
-                                    creadoen: new Date().toLocaleDateString('es-AR').split('T')[0],
-                                    usuario: {
-                                        connect: {
-                                            id: Number(idUsuario)
-                                        }
-                                    },
-                                }
-                            }
-                        }
-                    }
-                }
-            })
-            return alumno
-        }
-        else if (esDocente) {
-            let docente = await db.usuario.create({
-                data: {
-                    login: login,
-                    nombre: nombre,
-                    apellido: apellido,
-                    legajo: legajo,
-                    correo: correo,
-                    localidad: localidad,
-                    telefono: telefono,
-                    direccion: direccion,
-                    idrol: Number(idRol),
-                    sexo: sexo,
-                    password: contrasenia,
-                }
-            })
-            idMaterias?.map(async (idMateria) => {
-                const docentexmateria = await db.docentexmateria.create({
-                    data: {
-                        materia: {
-                            connect: {
-                                id: idMateria
-                            }
-                        },
-                        usuario: {
-                            connect: {
-                                id: docente.id
-                            }
-                        },
-                        asistenciadocente: {
-                            create: {
-                                presente: false,
-                                ausente: false,
-                                ausentejustificado: false,
-                                llegadatarde: false,
-                                llegadatardejustificada: false,
-                                mediafalta: false,
-                                mediafaltajustificada: false,
-                                creadoen: new Date().toLocaleDateString('es-AR').split('T')[0],
-                                usuario: {
-                                    connect: {
-                                        id: Number(idUsuario)
-                                    }
-                                },
-                            }
-                        }
-                    }
-                })
-                console.log(docentexmateria);
-            })
-            return docente
-        } else {
-            let usuario = await db.usuario.create({
-                data: {
-                    login: login,
-                    nombre: nombre,
-                    apellido: apellido,
-                    legajo: legajo,
-                    correo: correo,
-                    localidad: localidad,
-                    telefono: telefono,
-                    direccion: direccion,
-                    idrol: Number(idRol),
-                    sexo: sexo,
-                    password: contrasenia,
-                }
-            })
-            return usuario
-        }
-    } catch (error) {
-        console.log(error);
+  login,
+  nombre,
+  apellido,
+  correo,
+  legajo,
+  telefono,
+  localidad,
+  direccion,
+  idRol,
+  idTutor = 0,
+  contrasenia,
+  sexo,
+  idCurso = 0,
+  idUsuario,
+  esAlumno = false,
+  esDocente = false,
+  idMaterias
+) {
+  try {
+    if (esAlumno) {
+      const estadoalumno = await db.estadoalumno.findFirst({
+        where: {
+          estado: "Regular",
+        },
+      });
+      let alumno = await db.usuario.create({
+        data: {
+          login: login,
+          nombre: nombre,
+          apellido: apellido,
+          legajo: legajo,
+          correo: correo,
+          localidad: localidad,
+          telefono: telefono,
+          direccion: direccion,
+          idrol: Number(idRol),
+          sexo: sexo,
+          password: contrasenia,
+          alumnoxcursoxdivision: {
+            create: {
+              tutor: {
+                connect: {
+                  id: Number(idTutor),
+                },
+              },
+              cursoxdivision: {
+                connect: {
+                  id: Number(idCurso),
+                },
+              },
+              estadoalumno: {
+                connect: {
+                  id: Number(estadoalumno?.id),
+                },
+              },
+              fechamatriculacion: new Date()
+                .toLocaleDateString("es-AR")
+                .split("T")[0],
+              asistencia: {
+                create: {
+                  presente: false,
+                  ausente: false,
+                  ausentejustificado: false,
+                  llegadatarde: false,
+                  mediafalta: false,
+                  creadoen: new Date()
+                    .toLocaleDateString("es-AR")
+                    .split("T")[0],
+                  usuario: {
+                    connect: {
+                      id: Number(idUsuario),
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+      return alumno;
+    } else if (esDocente) {
+      let docente = await db.usuario.create({
+        data: {
+          login: login,
+          nombre: nombre,
+          apellido: apellido,
+          legajo: legajo,
+          correo: correo,
+          localidad: localidad,
+          telefono: telefono,
+          direccion: direccion,
+          idrol: Number(idRol),
+          sexo: sexo,
+          password: contrasenia,
+        },
+      });
+      idMaterias?.map(async (idMateria) => {
+        const docentexmateria = await db.docentexmateria.create({
+          data: {
+            materia: {
+              connect: {
+                id: idMateria,
+              },
+            },
+            usuario: {
+              connect: {
+                id: docente.id,
+              },
+            },
+            asistenciadocente: {
+              create: {
+                presente: false,
+                ausente: false,
+                ausentejustificado: false,
+                llegadatarde: false,
+                mediafalta: false,
+                creadoen: new Date().toLocaleDateString("es-AR").split("T")[0],
+                usuario: {
+                  connect: {
+                    id: Number(idUsuario),
+                  },
+                },
+              },
+            },
+          },
+        });
+        console.log(docentexmateria);
+      });
+      return docente;
+    } else {
+      let usuario = await db.usuario.create({
+        data: {
+          login: login,
+          nombre: nombre,
+          apellido: apellido,
+          legajo: legajo,
+          correo: correo,
+          localidad: localidad,
+          telefono: telefono,
+          direccion: direccion,
+          idrol: Number(idRol),
+          sexo: sexo,
+          password: contrasenia,
+        },
+      });
+      return usuario;
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
