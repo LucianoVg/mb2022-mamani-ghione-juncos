3652d6c31a50ce15c0f5f437c992ed6a23a56791
LucianoVg <lucianovelazquez19@gmail.com>
2023-05-13 00:26:28 -0300

    Update [...params].js

--- pages/api/gestion/docentes/usuario/[...params].js
+++ pages/api/gestion/docentes/usuario/[...params].js
@@ -60,7 +60,7 @@ export default async function handler(req, res) {
 
         const docente = await traerDocente(options)
         console.log(docente);
-        return res.status(200).json(docente, division)
+        return res.status(200).json(docente)
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: error.message })
