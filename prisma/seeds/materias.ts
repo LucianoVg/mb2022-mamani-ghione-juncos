c73fd0b468b1f82556f37608ee192ce33accdb19
LucianoVg <lucianovelazquez19@gmail.com>
2023-05-13 22:11:04 -0300

    Update materias.ts

--- prisma/seeds/materias.ts
+++ prisma/seeds/materias.ts
@@ -1,67 +1,65 @@
 export const materias = [
-    // {
-    //     id: 1,
-    //     nombre: "Lengua I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-    // {
-    //     id: 2,
-    //     nombre: "Matematica I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-    // {
-    //     id: 3,
-    //     nombre: "Biologia I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
+    {
+        id: 1,
+        nombre: "Lengua I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
+    {
+        id: 2,
+        nombre: "Matematica I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
+    {
+        id: 3,
+        nombre: "Biologia I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
 
-    // {
-    //     id: 4,
-    //     nombre: "Fisica I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-    // {
-    //     id: 5,
-    //     nombre: "Geografia I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-    // {
-    //     id: 6,
-    //     nombre: "Ingles I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-    // {
-    //     id: 7,
-    //     nombre: "Musica I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-    // {
-    //     id: 8,
-    //     nombre: "Educacion Tecnologica I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-    // {
-    //     id: 9,
-    //     nombre: "Ciudadania y Participacion I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-    // {
-    //     id: 10,
-    //     nombre: "Educacion Fisica I",
-    //     idCorrelativa: 0,
-    //     idCurso: 1
-    // },
-
-
+    {
+        id: 4,
+        nombre: "Fisica I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
+    {
+        id: 5,
+        nombre: "Geografia I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
+    {
+        id: 6,
+        nombre: "Ingles I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
+    {
+        id: 7,
+        nombre: "Musica I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
+    {
+        id: 8,
+        nombre: "Educacion Tecnologica I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
+    {
+        id: 9,
+        nombre: "Ciudadania y Participacion I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
+    {
+        id: 10,
+        nombre: "Educacion Fisica I",
+        idCorrelativa: 0,
+        idCurso: 1
+    },
 
 
 
@@ -69,66 +67,68 @@ export const materias = [
 
 
 
-    // {
-    //     id: 11,
-    //     nombre: "Lengua II",
-    //     idCorrelativa: 1,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 12,
-    //     nombre: "Matematica II",
-    //     idCorrelativa: 2,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 13,
-    //     nombre: "Quimica I",
-    //     idCorrelativa: 0,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 14,
-    //     nombre: "Biologia II",
-    //     idCorrelativa: 3,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 15,
-    //     nombre: "Ciencias Sociales",
-    //     idCorrelativa: 0,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 16,
-    //     nombre: "Ingles II",
-    //     idCorrelativa: 6,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 17,
-    //     nombre: "Teatro I",
-    //     idCorrelativa: 0,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 18,
-    //     nombre: "Educacion y Tecnologia II",
-    //     idCorrelativa: 8,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 19,
-    //     nombre: "Ciudadania y Participacion II",
-    //     idCorrelativa: 9,
-    //     idCurso: 2
-    // },
-    // {
-    //     id: 20,
-    //     nombre: "Educacion Fisica II",
-    //     idCorrelativa: 10,
-    //     idCurso: 2
-    // },
+
+
+    {
+        id: 11,
+        nombre: "Lengua II",
+        idCorrelativa: 1,
+        idCurso: 2
+    },
+    {
+        id: 12,
+        nombre: "Matematica II",
+        idCorrelativa: 2,
+        idCurso: 2
+    },
+    {
+        id: 13,
+        nombre: "Quimica I",
+        idCorrelativa: 0,
+        idCurso: 2
+    },
+    {
+        id: 14,
+        nombre: "Biologia II",
+        idCorrelativa: 3,
+        idCurso: 2
+    },
+    {
+        id: 15,
+        nombre: "Ciencias Sociales",
+        idCorrelativa: 0,
+        idCurso: 2
+    },
+    {
+        id: 16,
+        nombre: "Ingles II",
+        idCorrelativa: 6,
+        idCurso: 2
+    },
+    {
+        id: 17,
+        nombre: "Teatro I",
+        idCorrelativa: 0,
+        idCurso: 2
+    },
+    {
+        id: 18,
+        nombre: "Educacion y Tecnologia II",
+        idCorrelativa: 8,
+        idCurso: 2
+    },
+    {
+        id: 19,
+        nombre: "Ciudadania y Participacion II",
+        idCorrelativa: 9,
+        idCurso: 2
+    },
+    {
+        id: 20,
+        nombre: "Educacion Fisica II",
+        idCorrelativa: 10,
+        idCurso: 2
+    },
 
 
 
@@ -136,72 +136,72 @@ export const materias = [
 
 
 
-    // {
-    //     id: 21,
-    //     nombre: "Lengua III",
-    //     idCorrelativa: 11,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 22,
-    //     nombre: "Matematica III",
-    //     idCorrelativa: 12,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 23,
-    //     nombre: "Fisica II",
-    //     idCorrelativa: 4,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 24,
-    //     nombre: "Quimica II",
-    //     idCorrelativa: 13,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 25,
-    //     nombre: "Geografia II",
-    //     idCorrelativa: 5,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 26,
-    //     nombre: "Historia I",
-    //     idCorrelativa: 10,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 27,
-    //     nombre: "Ingles III",
-    //     idCorrelativa: 16,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 28,
-    //     nombre: "Musica II",
-    //     idCorrelativa: 7,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 29,
-    //     nombre: "Educacion Tecnologica II",
-    //     idCorrelativa: 8,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 30,
-    //     nombre: "Formacion para la vida y el trabajo I",
-    //     idCorrelativa: 0,
-    //     idCurso: 3
-    // },
-    // {
-    //     id: 31,
-    //     nombre: "Educacion Fisica III",
-    //     idCorrelativa: 20,
-    //     idCurso: 3
-    // },
+    {
+        id: 21,
+        nombre: "Lengua III",
+        idCorrelativa: 11,
+        idCurso: 3
+    },
+    {
+        id: 22,
+        nombre: "Matematica III",
+        idCorrelativa: 12,
+        idCurso: 3
+    },
+    {
+        id: 23,
+        nombre: "Fisica II",
+        idCorrelativa: 4,
+        idCurso: 3
+    },
+    {
+        id: 24,
+        nombre: "Quimica II",
+        idCorrelativa: 13,
+        idCurso: 3
+    },
+    {
+        id: 25,
+        nombre: "Geografia II",
+        idCorrelativa: 5,
+        idCurso: 3
+    },
+    {
+        id: 26,
+        nombre: "Historia I",
+        idCorrelativa: 10,
+        idCurso: 3
+    },
+    {
+        id: 27,
+        nombre: "Ingles III",
+        idCorrelativa: 16,
+        idCurso: 3
+    },
+    {
+        id: 28,
+        nombre: "Musica II",
+        idCorrelativa: 7,
+        idCurso: 3
+    },
+    {
+        id: 29,
+        nombre: "Educacion Tecnologica II",
+        idCorrelativa: 8,
+        idCurso: 3
+    },
+    {
+        id: 30,
+        nombre: "Formacion para la vida y el trabajo I",
+        idCorrelativa: 0,
+        idCurso: 3
+    },
+    {
+        id: 31,
+        nombre: "Educacion Fisica III",
+        idCorrelativa: 20,
+        idCurso: 3
+    },
 
 
 
@@ -209,78 +209,78 @@ export const materias = [
 
 
 
-    // {
-    //     id: 32,
-    //     nombre: "Lengua IV",
-    //     idCorrelativa: 21,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 33,
-    //     nombre: "Matematica IV",
-    //     idCorrelativa: 22,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 34,
-    //     nombre: "Fisica III",
-    //     idCorrelativa: 23,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 35,
-    //     nombre: "Quimica III",
-    //     idCorrelativa: 24,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 36,
-    //     nombre: "Biologia III",
-    //     idCorrelativa: 14,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 37,
-    //     nombre: "Geografia III",
-    //     idCorrelativa: 25,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 38,
-    //     nombre: "Historia II",
-    //     idCorrelativa: 26,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 39,
-    //     nombre: "Ingles IV",
-    //     idCorrelativa: 27,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 40,
-    //     nombre: "Artes Visuales",
-    //     idCorrelativa: 0,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 41,
-    //     nombre: "Formacion para la vida y el trabajo II",
-    //     idCorrelativa: 30,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 42,
-    //     nombre: "Educacion para la salud I",
-    //     idCorrelativa: 0,
-    //     idCurso: 4
-    // },
-    // {
-    //     id: 43,
-    //     nombre: "Educacion Fisica IV",
-    //     idCorrelativa: 31,
-    //     idCurso: 4
-    // },
+    {
+        id: 32,
+        nombre: "Lengua IV",
+        idCorrelativa: 21,
+        idCurso: 4
+    },
+    {
+        id: 33,
+        nombre: "Matematica IV",
+        idCorrelativa: 22,
+        idCurso: 4
+    },
+    {
+        id: 34,
+        nombre: "Fisica III",
+        idCorrelativa: 23,
+        idCurso: 4
+    },
+    {
+        id: 35,
+        nombre: "Quimica III",
+        idCorrelativa: 24,
+        idCurso: 4
+    },
+    {
+        id: 36,
+        nombre: "Biologia III",
+        idCorrelativa: 14,
+        idCurso: 4
+    },
+    {
+        id: 37,
+        nombre: "Geografia III",
+        idCorrelativa: 25,
+        idCurso: 4
+    },
+    {
+        id: 38,
+        nombre: "Historia II",
+        idCorrelativa: 26,
+        idCurso: 4
+    },
+    {
+        id: 39,
+        nombre: "Ingles IV",
+        idCorrelativa: 27,
+        idCurso: 4
+    },
+    {
+        id: 40,
+        nombre: "Artes Visuales",
+        idCorrelativa: 0,
+        idCurso: 4
+    },
+    {
+        id: 41,
+        nombre: "Formacion para la vida y el trabajo II",
+        idCorrelativa: 30,
+        idCurso: 4
+    },
+    {
+        id: 42,
+        nombre: "Educacion para la salud I",
+        idCorrelativa: 0,
+        idCurso: 4
+    },
+    {
+        id: 43,
+        nombre: "Educacion Fisica IV",
+        idCorrelativa: 31,
+        idCurso: 4
+    },
 
 
 
@@ -288,90 +288,90 @@ export const materias = [
 
 
 
-    // {
-    //     id: 44,
-    //     nombre: "Lengua V",
-    //     idCorrelativa: 32,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 45,
-    //     nombre: "Matematica V",
-    //     idCorrelativa: 33,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 46,
-    //     nombre: "Fisica IV",
-    //     idCorrelativa: 34,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 47,
-    //     nombre: "Quimica IV",
-    //     idCorrelativa: 35,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 48,
-    //     nombre: "Biologia IV",
-    //     idCorrelativa: 36,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 49,
-    //     nombre: "Geografia IV",
-    //     idCorrelativa: 37,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 50,
-    //     nombre: "Historia III",
-    //     idCorrelativa: 38,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 51,
-    //     nombre: "Ingles V",
-    //     idCorrelativa: 39,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 52,
-    //     nombre: "Musica",
-    //     idCorrelativa: 28,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 53,
-    //     nombre: "Psicologia",
-    //     idCorrelativa: 0,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 54,
-    //     nombre: "Formacion para la vida y el trabajo III",
-    //     idCorrelativa: 41,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 55,
-    //     nombre: "Educacion para la salud II",
-    //     idCorrelativa: 42,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 56,
-    //     nombre: "Ciencias de la Tierra",
-    //     idCorrelativa: 0,
-    //     idCurso: 5
-    // },
-    // {
-    //     id: 57,
-    //     nombre: "Educacion Fisica V",
-    //     idCorrelativa: 43,
-    //     idCurso: 5
-    // },
+    {
+        id: 44,
+        nombre: "Lengua V",
+        idCorrelativa: 32,
+        idCurso: 5
+    },
+    {
+        id: 45,
+        nombre: "Matematica V",
+        idCorrelativa: 33,
+        idCurso: 5
+    },
+    {
+        id: 46,
+        nombre: "Fisica IV",
+        idCorrelativa: 34,
+        idCurso: 5
+    },
+    {
+        id: 47,
+        nombre: "Quimica IV",
+        idCorrelativa: 35,
+        idCurso: 5
+    },
+    {
+        id: 48,
+        nombre: "Biologia IV",
+        idCorrelativa: 36,
+        idCurso: 5
+    },
+    {
+        id: 49,
+        nombre: "Geografia IV",
+        idCorrelativa: 37,
+        idCurso: 5
+    },
+    {
+        id: 50,
+        nombre: "Historia III",
+        idCorrelativa: 38,
+        idCurso: 5
+    },
+    {
+        id: 51,
+        nombre: "Ingles V",
+        idCorrelativa: 39,
+        idCurso: 5
+    },
+    {
+        id: 52,
+        nombre: "Musica",
+        idCorrelativa: 28,
+        idCurso: 5
+    },
+    {
+        id: 53,
+        nombre: "Psicologia",
+        idCorrelativa: 0,
+        idCurso: 5
+    },
+    {
+        id: 54,
+        nombre: "Formacion para la vida y el trabajo III",
+        idCorrelativa: 41,
+        idCurso: 5
+    },
+    {
+        id: 55,
+        nombre: "Educacion para la salud II",
+        idCorrelativa: 42,
+        idCurso: 5
+    },
+    {
+        id: 56,
+        nombre: "Ciencias de la Tierra",
+        idCorrelativa: 0,
+        idCurso: 5
+    },
+    {
+        id: 57,
+        nombre: "Educacion Fisica V",
+        idCorrelativa: 43,
+        idCurso: 5
+    },
 
 
 
