35b6cb0878633567489d4d38157636862c70a5f2
juncos22 <juncosnicolas36@gmail.com>
2023-05-15 18:11:13 -0300

    detalles mejorados

--- pages/reportes/conteo_notas.jsx
+++ pages/reportes/conteo_notas.jsx
@@ -1,4 +1,14 @@
-import { Card, CardActions, CardContent, Autocomplete, TextField, Box, CardHeader, Container, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Autocomplete,
+  TextField,
+  Box,
+  Container,
+  FormControl,
+  Grid,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -9,212 +19,209 @@ import { Layout } from "../../components/layout";
 import Loading from "../../components/loading";
 
 export default function Dashboard() {
-    const [usuario, setUsuario] = useState({ rol: '' })
-    const router = useRouter()
-    const [conteoNotasA, setConteoNotasA] = useState([])
-    const [conteoNotasB, setConteoNotasB] = useState([])
-    const [mejoresPromedios, setMejoresPromedios] = useState([])
-    const [cargando, setCargando] = useState(false)
-    const [cargando2, setCargando2] = useState(false)
-    const { loading, authUser } = useAuth()
-    const [materias, setMaterias] = useState([])
-    const [docenteA, setDocenteA] = useState([])
-    const [docenteB, setDocenteB] = useState([])
-    // const [idDivision, setIdDivision] = useState([])
-    const [idMateria, setIdMateria] = useState([])
-    useEffect(() => {
-        if (!loading && !authUser) {
-            router.push('/gestion/cuenta/login')
-        }
-        // traerUsuario()
-        if (authUser?.rol) {
-            if (!tienePermisos()) {
-                router.push('/error')
-            } else {
-                traerMaterias()
-                traerConteoNotasA()
-                traerConteoNotasB()
-                traerDocenteA()
-                traerDocenteB()
-            }
-        }
-    }, [loading, authUser, authUser?.rol?.tipo])
+  const router = useRouter();
+  const [conteoNotasA, setConteoNotasA] = useState([]);
+  const [conteoNotasB, setConteoNotasB] = useState([]);
+  const [cargando, setCargando] = useState(false);
+  const { loading, authUser } = useAuth();
+  const [materias, setMaterias] = useState([]);
+  const [docenteA, setDocenteA] = useState();
+  const [docenteB, setDocenteB] = useState();
 
-    const tienePermisos = () => {
-        return authUser?.rol?.tipo === 'Administrador'
-            || authUser?.rol?.tipo === 'Director'
+  useEffect(() => {
+    if (!loading && !authUser) {
+      router.push("/gestion/cuenta/login");
     }
-    const traerMaterias = async () => {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/materias`)
+    if (authUser && authUser?.rol) {
+      if (!tienePermisos()) {
+        router.push("/error");
+      } else {
+        traerMaterias();
+        traerConteoNotasA();
+        traerConteoNotasB();
+        traerDocenteA();
+        traerDocenteB();
+      }
+    }
+  }, [loading, authUser, authUser?.rol?.tipo]);
 
-        if (res.status === 200) {
-            setMaterias(res.data)
-            console.log(res.data);
-        }
-    }
-    const materiaSinRepetir = materias.filter(m => m.cursoxdivision?.iddivision === 1)
-    const materiasOrdenadas = materiaSinRepetir?.sort(
-        (a, b) => (
-            a.materiaxcursoxdivision?.idmateria - b.materiaxcursoxdivision?.idmateria
-        )
+  const tienePermisos = () => {
+    return (
+      authUser?.rol?.tipo === "Administrador" ||
+      authUser?.rol?.tipo === "Director"
+    );
+  };
+  const traerMaterias = async () => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/materias`
     );
 
-    // const traerUsuario = async () => {
-    //     const res = await axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/cuenta/${authUser?.email}`)
-    //     if (res.data) {
-    //         console.log(res.data);
-    //         setUsuario({ id: res.data?.id, rol: res.data?.rol?.tipo })
-    //     }
-    // }
-    const traerConteoNotasA = async (idMateria) => {
-        setCargando(true)
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/reportes/notas/contador_notas/a/${idMateria || 1}`)
-        console.log(res.data);
-        if (res.status === 200) {
-            setConteoNotasA(res.data)
-        }
-        setCargando(false)
+    if (res.status === 200) {
+      setMaterias(res.data);
+      console.log(res.data);
     }
-    const traerConteoNotasB = async (idMateria) => {
-        setCargando(true)
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/reportes/notas/contador_notas/b/${idMateria || 1}`)
-        console.log(res.data);
-        if (res.status === 200) {
-            setConteoNotasB(res.data)
-        }
-        setCargando(false)
+  };
+  const materiaSinRepetir = materias.filter(
+    (m) => m.cursoxdivision?.iddivision === 1
+  );
+  const materiasOrdenadas = materiaSinRepetir?.sort(
+    (a, b) =>
+      a.materiaxcursoxdivision?.idmateria - b.materiaxcursoxdivision?.idmateria
+  );
+
+  const traerConteoNotasA = async (idMateria) => {
+    setCargando(true);
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/reportes/notas/contador_notas/a/${
+        idMateria || 1
+      }`
+    );
+    console.log(res.data);
+    if (res.status === 200) {
+      setConteoNotasA(res.data);
     }
-    const traerDocenteA = async (materiaId, division = "A") => {
-        const res = await axios.get(
-            `${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/docentes/usuario/${materiaId || 1}/${division}`
-        );
-        if (res.status === 200 && res.data) {
-            setDocenteA(res.data);
-            console.log("DOCENTE AAA", docenteA);
-        }
-    };
-    const traerDocenteB = async (materiaId, division = "B") => {
-        const res = await axios.get(
-            `${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/docentes/usuario/${materiaId || 1}/${division}`
-        );
-        if (res.status === 200 && res.data) {
-            setDocenteB(res.data);
-
-            console.log("DOCENTE BBB", docenteB);
-        }
-    };
-    const handleSelect = (e) => {
-        traerConteoNotasA(Number(e.target.value))
-        traerConteoNotasB(Number(e.target.value))
-        traerDocenteA(Number(e.target.value))
-        traerDocenteB(Number(e.target.value))
-
-
+    setCargando(false);
+  };
+  const traerConteoNotasB = async (idMateria) => {
+    setCargando(true);
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/reportes/notas/contador_notas/b/${
+        idMateria || 1
+      }`
+    );
+    console.log(res.data);
+    if (res.status === 200) {
+      setConteoNotasB(res.data);
     }
+    setCargando(false);
+  };
+  const traerDocenteA = async (materiaId, division = "A") => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/docentes/usuario/${
+        materiaId || 1
+      }/${division}`
+    );
+    if (res.status === 200 && res.data) {
+      setDocenteA(res.data);
+      console.log("DOCENTE AAA", docenteA);
+    }
+  };
+  const traerDocenteB = async (materiaId, division = "B") => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/docentes/usuario/${
+        materiaId || 1
+      }/${division}`
+    );
+    if (res.status === 200 && res.data) {
+      setDocenteB(res.data);
 
+      console.log("DOCENTE BBB", docenteB);
+    }
+  };
+  const handleSelect = (e) => {
+    traerConteoNotasA(Number(e.target.value));
+    traerConteoNotasB(Number(e.target.value));
+    traerDocenteA(Number(e.target.value));
+    traerDocenteB(Number(e.target.value));
+  };
 
+  return (
+    <Layout>
+      <Typography variant="h5" sx={{ mb: 2 }}>
+        Reporte Conteo de Notas
+      </Typography>
 
-    return (
-        <Layout>
-            <Typography variant="h5" sx={{ mb: 2 }}>Reporte Conteo de Notas</Typography>
-
-            <Grid container spacing={2} flex>
-                <Grid item xs={12}>
-                    {materias.length > 0 &&
-                        (
-                            <FormControl sx={{ marginBottom: "20px" }}>
-                                <Autocomplete
-                                    sx={{ width: "330px" }}
-                                    disablePortal
-                                    id="inputMateria"
-                                    // value={value}
-                                    name="idMateria"
-                                    onChange={handleSelect}
-                                    options={materiasOrdenadas}
-                                    getOptionLabel={(materia) =>
-                                        `${materia?.materia?.nombre}`
-                                    }
-                                    isOptionEqualToValue={(option, value) => {
-                                        return option?.materia?.id === value?.materia?.id;
-                                    }}
-                                    noOptionsText={"No existe materia con ese nombre"}
-                                    renderOption={(props, materia) => (
-                                        <Box
-                                            component="li"
-                                            {...props}
-                                            key={materia?.id}
-                                            value={materia?.materia?.id}
-                                        >
-                                            {materia?.materia?.nombre}
-                                        </Box>
-                                    )}
-                                    renderInput={(params) => (
-                                        <TextField {...params} label="Materias" />
-                                    )}
-                                />
-                            </FormControl>
-                        )
-                    }
-                </Grid>
-                <Grid item xs={6}>
-                    {
-                        !cargando && conteoNotasA.length > 0 && (
-                            <Box>
-                                <Box sx={{ marginBottom: "20px" }}>
-                                    <Typography variant="h6" textAlign='center' >
-                                        {
-                                            `Docente: ${docenteA.usuario?.nombre} ${docenteA.usuario?.apellido}`
-                                        }
-                                    </Typography>
-                                </Box>
-                                <Card sx={{ backgroundColor: '#f9f9f9', minWidth: "400px", minHeight: "200px" }}>
-                                    <CardContent>
-                                        <BarChartA
-                                            data={conteoNotasA}
-                                        />
-                                    </CardContent>
-                                </Card>
-                            </Box>
-                        )
-                    }
-                    {
-                        cargando && (
-                            <Container sx={{ textAlign: 'center' }}>
-                                <Loading size={50} />
-                            </Container>
-                        )
-                    }
-                </Grid>
-                <Grid item xs={6}>
-                    {
-                        !cargando && conteoNotasB.length > 0 && (
-                            <Box>
-                                <Box sx={{ marginBottom: "20px" }}>
-                                    <Typography variant="h6" textAlign='center'>
-                                        {
-                                            `Docente: ${docenteB.usuario?.nombre} ${docenteB.usuario?.apellido}`
-                                        }
-                                    </Typography>
-                                </Box>
-                                <Card sx={{ backgroundColor: '#f9f9f9', minWidth: "400px", minHeight: "200px" }}>
-                                    <CardContent>
-                                        <BarChartB
-                                            data={conteoNotasB}
-                                        />
-                                    </CardContent>
-                                </Card>
-                            </Box>
-                        )
-                    }
-                    {
-                        cargando && (
-                            <Container sx={{ textAlign: 'center' }}>
-                                <Loading size={50} />
-                            </Container>
-                        )
-                    }
-                </Grid>
-            </Grid>
-        </Layout>
-    )
+      <Grid container spacing={2} flex>
+        <Grid item xs={12}>
+          {materias.length > 0 && (
+            <FormControl sx={{ marginBottom: "20px" }}>
+              <Autocomplete
+                sx={{ width: "330px" }}
+                disablePortal
+                id="inputMateria"
+                // value={value}
+                name="idMateria"
+                onChange={handleSelect}
+                options={materiasOrdenadas}
+                getOptionLabel={(materia) => `${materia?.materia?.nombre}`}
+                isOptionEqualToValue={(option, value) => {
+                  return option?.materia?.id === value?.materia?.id;
+                }}
+                noOptionsText={"No existe materia con ese nombre"}
+                renderOption={(props, materia) => (
+                  <Box
+                    component="li"
+                    {...props}
+                    key={materia?.id}
+                    value={materia?.materia?.id}
+                  >
+                    {materia?.materia?.nombre}
+                  </Box>
+                )}
+                renderInput={(params) => (
+                  <TextField {...params} label="Materias" />
+                )}
+              />
+            </FormControl>
+          )}
+        </Grid>
+        <Grid item xs={6}>
+          {!cargando && conteoNotasA.length > 0 && (
+            <Box>
+              <Box sx={{ marginBottom: "20px" }}>
+                <Typography variant="h6" textAlign="center">
+                  {docenteA &&
+                    `Docente: ${docenteA.usuario?.nombre} ${docenteA.usuario?.apellido}`}
+                </Typography>
+              </Box>
+              <Card
+                sx={{
+                  backgroundColor: "#f9f9f9",
+                  minWidth: "400px",
+                  minHeight: "200px",
+                }}
+              >
+                <CardContent>
+                  <BarChartA data={conteoNotasA} />
+                </CardContent>
+              </Card>
+            </Box>
+          )}
+          {cargando && (
+            <Container sx={{ textAlign: "center" }}>
+              <Loading size={50} />
+            </Container>
+          )}
+        </Grid>
+        <Grid item xs={6}>
+          {!cargando && conteoNotasB.length > 0 && (
+            <Box>
+              <Box sx={{ marginBottom: "20px" }}>
+                <Typography variant="h6" textAlign="center">
+                  {docenteB &&
+                    `Docente: ${docenteB.usuario?.nombre} ${docenteB.usuario?.apellido}`}
+                </Typography>
+              </Box>
+              <Card
+                sx={{
+                  backgroundColor: "#f9f9f9",
+                  minWidth: "400px",
+                  minHeight: "200px",
+                }}
+              >
+                <CardContent>
+                  <BarChartB data={conteoNotasB} />
+                </CardContent>
+              </Card>
+            </Box>
+          )}
+          {cargando && (
+            <Container sx={{ textAlign: "center" }}>
+              <Loading size={50} />
+            </Container>
+          )}
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 }
--- pages/reportes/mejor_promedio.jsx
+++ pages/reportes/mejor_promedio.jsx
@@ -1,4 +1,4 @@
-import { Card, CardActions, CardContent, CardHeader, Container, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import { Card, CardContent, Container, Grid, Typography } from "@mui/material";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -9,109 +9,105 @@ import { Layout } from "../../components/layout";
 import Loading from "../../components/loading";
 
 export default function Dashboard() {
-    const [usuario, setUsuario] = useState({ rol: '' })
-    const router = useRouter()
-    const [conteoNotas, setConteoNotas] = useState([])
-    const [mejoresPromediosA, setmejoresPromediosA] = useState([])
-    const [mejoresPromediosB, setmejoresPromediosB] = useState([])
-    const [cargando, setCargando] = useState(false)
-    const [cargando2, setCargando2] = useState(false)
-    const { loading, authUser } = useAuth()
-    const [materias, setMaterias] = useState([])
+  const router = useRouter();
+  const [mejoresPromediosA, setmejoresPromediosA] = useState([]);
+  const [mejoresPromediosB, setmejoresPromediosB] = useState([]);
+  const [cargando, setCargando] = useState(false);
+  const { loading, authUser } = useAuth();
 
-    useEffect(() => {
-        if (!loading && !authUser) {
-            router.push('/gestion/cuenta/login')
-        }
-        // traerUsuario()
-        if (authUser?.rol) {
-            if (!tienePermisos()) {
-                router.push('/error')
-            } else {
-                traermejoresPromediosA()
-                traermejoresPromediosB()
-            }
-        }
-    }, [loading, authUser, authUser?.rol?.tipo])
+  useEffect(() => {
+    if (!loading && !authUser) {
+      router.push("/gestion/cuenta/login");
+    }
+    if (authUser && authUser?.rol) {
+      if (!tienePermisos()) {
+        router.push("/error");
+      } else {
+        traermejoresPromediosA();
+        traermejoresPromediosB();
+      }
+    }
+  }, [loading, authUser, authUser?.rol?.tipo]);
 
-    const tienePermisos = () => {
-        return authUser?.rol?.tipo === 'Administrador'
-            || authUser?.rol?.tipo === 'Director'
-    }
-    // const traerUsuario = async () => {
-    //     const res = await axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/gestion/cuenta/${authUser?.email}`)
-    //     if (res.data) {
-    //         console.log(res.data);
-    //         setUsuario({ id: res.data?.id, rol: res.data?.rol?.tipo })
-    //     }
-    // }
-
-    const traermejoresPromediosA = async () => {
-        setCargando2(true)
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/reportes/notas/mejor_promedio/a`)
-        console.log(res.data);
-        if (res.status === 200) {
-            setmejoresPromediosA(res.data)
-        }
-        setCargando2(false)
-    }
-    const traermejoresPromediosB = async () => {
-        setCargando2(true)
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_CLIENT_URL}/reportes/notas/mejor_promedio/b`)
-        console.log(res.data);
-        if (res.status === 200) {
-            setmejoresPromediosB(res.data)
-        }
-        setCargando2(false)
-    }
-    const handleSelect = (e) => {
-        traerConteoNotas(Number(e.target.value))
-    }
+  const tienePermisos = () => {
     return (
-        <Layout>
-            <Typography variant="h4" sx={{ mb: 2 }}>Reporte Mejores Promedios</Typography>
-            <Grid container spacing={2} flex>
-                <Grid item xs={6}>
-                    {
-                        !cargando2 && mejoresPromediosA.length > 0 && (
-                            <Card sx={{ backgroundColor: '#f9f9f9', minWidth: "400px", minHeight: "200px" }}>
-                                <CardContent>
-                                    <PieChartA
-                                        data={mejoresPromediosA}
-                                    />
-                                </CardContent>
-                            </Card>
-                        )
-                    }
-                    {
-                        cargando && (
-                            <Container sx={{ textAlign: 'center' }}>
-                                <Loading size={50} />
-                            </Container>
-                        )
-                    }
-                </Grid>
-                <Grid item xs={6}>
-                    {
-                        !cargando2 && mejoresPromediosB.length > 0 && (
-                            <Card sx={{ backgroundColor: '#f9f9f9', minWidth: "400px", minHeight: "200px" }}>
-                                <CardContent>
-                                    <PieChartB
-                                        data={mejoresPromediosB}
-                                    />
-                                </CardContent>
-                            </Card>
-                        )
-                    }
-                    {
-                        cargando2 && (
-                            <Container sx={{ textAlign: 'center' }}>
-                                <Loading size={50} />
-                            </Container>
-                        )
-                    }
-                </Grid>
-            </Grid>
-        </Layout>
-    )
+      authUser?.rol?.tipo === "Administrador" ||
+      authUser?.rol?.tipo === "Director"
+    );
+  };
+
+  const traermejoresPromediosA = async () => {
+    setCargando(true);
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/reportes/notas/mejor_promedio/a`
+    );
+    console.log(res.data);
+    if (res.status === 200) {
+      setmejoresPromediosA(res.data);
+    }
+    setCargando(false);
+  };
+  const traermejoresPromediosB = async () => {
+    setCargando(true);
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/reportes/notas/mejor_promedio/b`
+    );
+    console.log(res.data);
+    if (res.status === 200) {
+      setmejoresPromediosB(res.data);
+    }
+    setCargando(false);
+  };
+  const handleSelect = (e) => {
+    traerConteoNotas(Number(e.target.value));
+  };
+  return (
+    <Layout>
+      <Typography variant="h4" sx={{ mb: 2 }}>
+        Reporte Mejores Promedios
+      </Typography>
+      <Grid container spacing={2} flex>
+        <Grid item xs={6}>
+          {!cargando && mejoresPromediosA.length > 0 && (
+            <Card
+              sx={{
+                backgroundColor: "#f9f9f9",
+                minWidth: "400px",
+                minHeight: "200px",
+              }}
+            >
+              <CardContent>
+                <PieChartA data={mejoresPromediosA} />
+              </CardContent>
+            </Card>
+          )}
+          {cargando && (
+            <Container sx={{ textAlign: "center" }}>
+              <Loading size={50} />
+            </Container>
+          )}
+        </Grid>
+        <Grid item xs={6}>
+          {!cargando && mejoresPromediosB.length > 0 && (
+            <Card
+              sx={{
+                backgroundColor: "#f9f9f9",
+                minWidth: "400px",
+                minHeight: "200px",
+              }}
+            >
+              <CardContent>
+                <PieChartB data={mejoresPromediosB} />
+              </CardContent>
+            </Card>
+          )}
+          {cargando && (
+            <Container sx={{ textAlign: "center" }}>
+              <Loading size={50} />
+            </Container>
+          )}
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 }
